ed8285c6942a02a5f058e9ab35f0f73031c4ff58
Maria Fernanda Quintero Gaona <m.quintero@globant.com>
2025-01-23 15:40:15 -0500

    uploaded streamlit app

---
+++ MultimediaReach/main.py
@@ -0,0 +1,162 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import numpy as np
+
+
+
+from utils import  (first_row_calculation, alcance_standart_calculation, index_dup,
+                     alcance_ajustado_calculation, plot_preprocessing,create_annotations)
+
+
+st. title("Multimedia Reach")
+#Listas con los targets de referencia
+targets = ["Personas", "Amas", "HM +25", "HM +25 nse 3-6", "HM +25 nse 2-3", 
+           "HM +25 nse 4-6", "HM +18", "HM 12-24", "HM 25 -39"]
+#Lista con las frecuencias de referencia
+frecuencias_lst = ["1+", "2+", "3+", "4+" , "5+" , "6+" , "7+" ]
+#Lista de medios
+medios_lst =  ["Tv Nacional", "Tv Regional", "Tv Suscripción", "Radio", "Prensa", "Prensa OnLine",
+               "Revistas", "Revistas On Line", "T. Digital", "Cine", "Google Search", "Facebook", 
+                "Instagram", "Tik Tok", "Twitter", "Youtube", "Spotify", "Vallas", "Medios de Transporte",
+                "Paraderos", "Valla Movil", "Taxi", "Centros Comerciales", "Aeropuertos"]
+
+#Inicializacion del session state
+widgets = {
+    'selected_freq': "1+",  # Valor predeterminado para el campo de texto
+    'selected_target': "Personas"  # Valor predeterminado para el slider
+}
+for widget_name, default_value in widgets.items():
+    if widget_name not in st.session_state:
+        st.session_state[widget_name] = default_value
+
+#Ingreso de la frecuencia y el target
+col1, col2 = st.columns(2)
+with col1:
+    st.markdown("### Frecuencia de alcance")
+    selected_freq = st.selectbox("Selecciona la frecuencia de alcance", frecuencias_lst)
+with col2:
+    st.markdown("### Target de referencia")
+    selected_target = st.selectbox("Selecciona el target de referencia", targets)
+
+#Define start dataframe
+start_df= pd.DataFrame({"Medio":["Tv Nacional", "Radio", "Youtube"] , 
+                        "Porcentaje": [41.0, 11.0,14.0],
+                        "Alcance Multimedia Standart": [None, None,None],
+                        "Alcance Multimedia Ajustado":[None, None,None],
+                         "Indice de duplicación": [None, None, None] })
+#First row calculations
+start_df = first_row_calculation(start_df, selected_freq)
+#Next rows calcularion
+start_df = alcance_standart_calculation(start_df, selected_freq)
+start_df = index_dup(start_df, selected_target)
+start_df = alcance_ajustado_calculation(start_df, selected_freq)
+
+    
+
+if 'start_df' not in st.session_state:
+    st.session_state['start_df'] = pd.DataFrame(start_df)
+    #st.session_state['start_df']['Porcentaje'] = pd.to_numeric(st.session_state['start_df']['Porcentaje'], errors='coerce')
+edited_df = st.data_editor(st.session_state['start_df'],
+                           column_config={
+                               "Medio": st.column_config.SelectboxColumn(
+                                   "Medio",
+                                   help="Medio de comunicación",
+                                   options=medios_lst,
+                                   default= medios_lst[0],
+                                   width=150,
+                               ),
+                               "Porcentaje": st.column_config.NumberColumn(
+                                   "Porcentaje",
+                                   help="Porcentaje de alcance",
+                                   min_value=0.0,
+                                   max_value=100,
+                                   #default=1,
+                                   width=100,
+                                   format= "%.2f"
+                               ),
+                               "Alcance Multimedia Standart": st.column_config.NumberColumn(
+                                   "Alcance Multimedia Standart",
+                                   help="Alcance standart",
+                                   width=180,
+                                   disabled= True,
+                                   format= "%.2f"
+                               ),
+                               "Alcance Multimedia Ajustado": st.column_config.NumberColumn(
+                                   "Alcance Multimedia Ajustado",
+                                   help="Alcande ajustado",
+                                   width=180,
+                                   disabled=True,
+                                   format= "%.2f"
+                               ),
+                               "Indice de duplicación": st.column_config.NumberColumn(
+                                   "Indice de duplicación",
+                                   help="Indice de duplicación",
+                                   width=150,
+                                   disabled=True,
+                                   format= "%.2f",
+                                   
+                               )
+                           },
+                           hide_index=True,
+                           column_order=("Medio", "Porcentaje", "Alcance Multimedia Standart", "Alcance Multimedia Ajustado"),
+                           num_rows="dynamic")
+
+if (st.session_state['start_df'] is not None) and ((not st.session_state['start_df'].equals(edited_df)) or ( st.session_state['selected_freq']!=selected_freq) or ( st.session_state['selected_target']!=selected_target)):
+    st.session_state['start_df'] = edited_df
+    st.session_state['selected_freq'] = selected_freq
+    st.session_state['selected_target'] = selected_target
+    st.session_state['start_df'] = first_row_calculation(st.session_state['start_df'], st.session_state['selected_freq'])
+    st.session_state['start_df'] = alcance_standart_calculation(st.session_state['start_df'], st.session_state['selected_freq'])
+    st.session_state['start_df'] = index_dup(st.session_state['start_df'], st.session_state['selected_target'])
+    st.session_state['start_df'] = alcance_ajustado_calculation(st.session_state['start_df'], st.session_state['selected_freq'])
+    st.rerun()
+
+
+grafica = st.checkbox("Mostrar graficas")
+if grafica:
+    if  st.session_state['start_df']['Porcentaje'].isna().any():
+        st.warning("Debe llenar todos los campos")
+        st.stop()
+    if  st.session_state['start_df']['Medio'].duplicated(keep=False).any():
+        st.warning("No deben haber medios duplicados")
+        st.stop()
+    #Plots
+    tab1, tab2 = st.tabs(["Alcance standart", "Alcance ajustado"])
+    # Gráfico de barras
+    with tab1:
+        colors = px.colors.qualitative.Prism
+        df2 = st.session_state['start_df'][['Medio', 'Alcance Multimedia Standart']]
+        df2 = plot_preprocessing(df2, 'Alcance Multimedia Standart')
+        fig = px.bar(df2, y="Medios acumulados", x = "Porcentaje", color="Medio",
+                    title='Alcance Multimedia Standart', orientation='h', text_auto=True,
+                     color_discrete_sequence=colors)
+        fig.update_traces(texttemplate='%{x:.2f}')
+        fig.update_layout(yaxis={'categoryorder': 'total descending'},
+                        xaxis=dict(range=[0, 100]),
+                        xaxis_title='Porcentaje de alcance multimedia standart',
+                        yaxis_title='Medios')
+        annotations = create_annotations(st.session_state['start_df'], df2, 'Alcance Multimedia Standart')
+        fig.update_layout(annotations=annotations)
+        # Mostrar en streamlit
+        st.plotly_chart(fig)
+
+    with tab2:
+        colors = px.colors.qualitative.Dark2
+        df2 = st.session_state['start_df'][['Medio', 'Alcance Multimedia Ajustado']]
+        df2 = plot_preprocessing(df2, 'Alcance Multimedia Ajustado')
+        fig = px.bar(df2, y="Medios acumulados", x = "Porcentaje", color="Medio",
+                    title='Alcance Multimedia Ajustado', orientation='h', text_auto=True,
+                    color_discrete_sequence=colors)
+        fig.update_traces(texttemplate='%{x:.2f}')
+        fig.update_layout(yaxis={'categoryorder': 'total descending'},
+                        xaxis=dict(range=[0, 100]),
+                        xaxis_title='Porcentaje de alcance multimedia ajustado',
+                        yaxis_title='Medios')
+        annotations = create_annotations(st.session_state['start_df'], df2, 'Alcance Multimedia Ajustado')
+        fig.update_layout(annotations=annotations)
+        # Mostrar en streamlit
+        st.plotly_chart(fig)
+
+
---
+++ MultimediaReach/utils.py
@@ -0,0 +1,117 @@
+import pandas as pd
+import streamlit as st
+
+
+df_permutaciones = pd.read_csv("datasets/permutationIndex.csv")
+
+df_frecuencias = pd.read_csv("datasets/frecuencias.csv")
+
+
+
+def get_permutation_index(perm):
+    try:
+        filtered_permutation= df_permutaciones[df_permutaciones['Permutaciones'] == perm]['INDICES'].iloc[0]
+        return float(filtered_permutation)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    
+
+
+def get_frecuencia(perm):
+    try:
+        filtered_frecuencia= df_frecuencias[df_frecuencias['Frecuencia de Alcance'] == perm]['proporcion'].values[0]
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    return float(filtered_frecuencia)
+
+
+def first_row_calculation(df: pd.DataFrame, selected_freq: str):
+    try:
+     #Calcular el valor de la primera fila
+     df.iloc[0, 2] = df.iloc[0,1]*get_frecuencia(selected_freq)
+     df.iloc[0, 3] = df.iloc[0,1]*get_frecuencia(selected_freq)
+    except Exception as e:
+       print(f"An error occurred: {e}")
+    return df
+
+def alcance_standart_calculation(df: pd.DataFrame, selected_freq: str):
+    try:
+     #Calcular el valor de la segunda filaen adelante
+     for i in range(1, len(df)):
+            if (df.iloc[i,0] is not None) and (df.iloc[i,1] is not None):
+                j=i-1
+                previous_value = df.iloc[j, 2]  # Get value from the previous row in the same column
+                adjustment = (100 - previous_value)/100 * df.iloc[i, 1] * get_frecuencia(selected_freq)
+                df.iloc[i, 2] = previous_value + adjustment
+            else:
+                df.iloc[i,2] = None
+     return df
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def index_dup(df: pd.DataFrame, selected_target: str):
+    try:
+        for i in range(1, len(df)):
+            if (df.iloc[i,0] is not None) and (df.iloc[i,1] is not None):
+                permutacion = selected_target + df.iloc[i,0]
+                df.iloc[i,4] =get_permutation_index(permutacion)
+            else:
+                df.iloc[i,4] = None
+        return df       
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    
+
+def alcance_ajustado_calculation(df: pd.DataFrame, selected_freq: str):
+    try:
+     #Calcular el valor de la segunda filaen adelante
+     for i in range(1, len(df)):
+            if (df.iloc[i,0] is not None) and (df.iloc[i,1] is not None):
+                j=i-1
+                previous_value = df.iloc[j, 3]  # Get value from the previous row in the same column
+                per1 = get_frecuencia(selected_freq)
+                per2 = 100- df.iloc[i,4]
+                per3 = (100-previous_value)/100
+                df.iloc[i, 3] = previous_value +(1-per1*per2/100)*per3*df.iloc[i,1]
+            else:
+                df.iloc[i,3] = None
+     return df
+    except Exception as e:
+       print(f"An error occurred: {e}")
+
+
+def plot_preprocessing(df: pd.DataFrame, alcance : str):
+    df['Porcentaje por medio'] = df[alcance] - df[alcance].shift()
+    df['Porcentaje por medio'].iloc[0] = df[alcance].iloc[0]
+    #Initialize the 'Medio acumulado' column
+    df['Medios Acumulados'] = ''
+    # Loop through each row to create the accumulated string
+    for i in range(len(df)):
+        if i == 0:
+            df.at[i, 'Medios Acumulados'] = df.at[i, 'Medio']  # First row just takes the current Medio
+        else:
+            df.at[i, 'Medios Acumulados'] = df.at[i-1, 'Medios Acumulados'] + '+' + df.at[i, 'Medio']
+
+    #df['Medios Acumulados'].iloc[0] = df['Medio'].iloc[0]
+    for index,medio in enumerate(df['Medio']):
+        df[medio] = 0
+        df[medio].iloc[index:len(df)]=df['Porcentaje por medio'].iloc[index]
+    df = df.loc[:, 'Medios Acumulados':]
+    df = df.set_index('Medios Acumulados').stack(level=-1)
+    df = df.reset_index()
+    df.columns= ["Medios acumulados", "Medio", "Porcentaje"]
+    return df
+
+
+def create_annotations(start_df: pd.DataFrame, df2:pd.DataFrame, column: str):
+    j=0
+    annotations = []
+    for i, row in start_df.iterrows():
+        annotations.append(dict(x=6, 
+                                y=df2['Medios acumulados'].iloc[j], 
+                                text=str(round(row[column],2))+"%", 
+                                font = dict(size=10, family="Arial Black", color = "black"),
+                                #textangle=-90,
+                                   showarrow=False))
+        j+=len(start_df)
+    return annotations
